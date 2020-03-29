5c80b78ebea31b486dcb60b753a33d19ce060e95
cihan_bas <Varidat65>
2020-03-29 17:29:27 +0300

    Initial Commit

---
+++ ArtBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtBook
+//
+//  Created by cihanbas on 29.03.2020.
+//  Copyright © 2020 cihanbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
